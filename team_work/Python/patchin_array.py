4112d1e6b1c2d0189707923cbf2d42e121d759ea
JairoTM-es <jairo.trujillo@encora.com>
2025-08-10 20:41:30 -0600

    patchin added

--- team_work/Python/patchin_array.py
+++ team_work/Python/patchin_array.py
@@ -1,60 +1,22 @@
-#aqui tenemos que ver cual es el que falta, por ejemplo, del ejemplo 1,5, 10 donde n=20 debemos de verificar primero cuales numeros no se pueden formar por si mismos, 
-#podemos validar ordenando una lista y despues de ordenarla ver si son iguales, pues tampoco se pueden repetir elementos, y no dejar que se agreguen a la lista de suma los valores que existen en mayores a n
+def patchesNeeded(array, n):
 
-
-array=[1,5, 10] 
-n = 20
-
-#desde aqui debemos ver cual es el que se pierde, entonces debemos generar las posibles sumas, desde agregando los mismos numeros del array original y despues generando las sumas 
-#entoncespara comparar podemos generar una lista con todos los valores de 1 hasta n,
-
-#generacion de 1 a n
-
-array_aux = list(range(1, n+1))
-#print(array_aux)
-
-#aqui va el que genera todas las sumas entre ellos, que seria como un ciclo, que el cilco va iterando
-list_list_aux=[]
-list_aux=[]
-i=len(array)
-for num in array:
     
-    list_aux.append(num)
-    #print("lista aux : " + str(list_aux))
-    aux_1=0
-    while aux_1 <=  i:
-        aux_1= aux_1+ 1
-        #print(aux_1)
 
+    faltantes = 1
+    i = 0
+    patches = 0
 
-##--------------------------
-possible = set([0])
-patches = 0 
-print("possible: " + str(possible))
-for num in array:
-    new_sum = set()
-    print("num: " + str(num))
-    for x in possible:
-        suma = x + num
-        print("suma=  " + str(x)  +" +" + str(num))
-        print("suma flag: " + str(suma))
-        if suma <= n:
-            new_sum.add(suma)
-            
-            print("new_sum: " + str(new_sum))
-    possible.update(new_sum)
-print(possible)
+    while faltantes <= n:
+        if i < len(array) and array[i] <= faltantes:
+            faltantes += array[i]
+            print("missing atches: " + str(faltantes))
+            i += 1
+        else:
+            patches += 1
+            faltantes += faltantes
 
-i =1
-while i <= n:
-    if i not in possible:
-        patches += 1
-        possible.add(i)
-        new_sum = set()
-        for x in possible:
-            suma = x + i
-            if suma <= n:
-                new_sum.add(suma)
-        possible.update(new_sum)
-    i += 1
-print("Patches needed: " + str(patches))
+    print("Patches needed: " + str(patches))
+
+array = [1, 3]
+n = 6
+patchesNeeded(array, n)
